6572e234c03d245405669f195b9cfcd9be44147d
Artemiy Mutovin <artemmutovin@mail.ru>
2022-04-08 07:57:11 -0400

    tests and questions resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :tests do
+    resources :questions
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
