bbedce48b3ef18db7794809f7b9e05a05eb420a0
Artemiy Mutovin <artemmutovin@mail.ru>
2022-04-26 04:56:10 -0400

    Answer files

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
   root to: 'tests#index'
 
   resources :tests do
     resources :questions, only: %i[index new create]
+
   end
 
-  resources :questions, only: %i[show edit update]
+  resources :questions, only: %i[show edit update] do
+    resources :answers
+  end
+
+  resources :answers
 end
