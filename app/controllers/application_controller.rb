0bba96b6c00a67000855fea197452bc623be9803
Artemiy Mutovin <artemmutovin@mail.ru>
2022-05-20 06:46:07 -0400

    Localization,Gemfile changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(user)
@@ -10,5 +11,15 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+  end
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Testguru
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ru
     config.time_zone = 'Ekaterinburg'
 
     # Configuration for the application, engines, and railties goes here.
